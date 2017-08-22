cf159a9a7f6ff4a0c1403a0b84f15334d135f68b
yangshouguo <891584158@qq.com>
2017-08-22 21:35:34 +0800

    it can get all nodes address and content of some certain nodes

--- Feature_Of_Binary.py
+++ Feature_Of_Binary.py
@@ -13,11 +13,53 @@ import sys,os
 class Process_with_Single_Function(object):
     def __init__(self, func_t):
         self._Blocks = set()
+        self._Blocks_list = []
         self._func = func_t
-        self._Edges = {}
+        self._block_boundary = {}
         self._addr_func = func_t.startEA #first address of function
         self._name_func = str(GetFunctionName(func_t.startEA)) # GetFunctionName(startEA) returns the function name
+        self._init_all_nodes()
     
+    # initial block_boundary , get every node's range of address
+    def _init_all_nodes(self):
+        
+        flowchart = FlowChart(self._func)
+        
+        for i in range(flowchart.size):
+            basicblock = flowchart.__getitem__(i)
+            self._Blocks.add(basicblock.startEA)
+            self._block_boundary[basicblock.startEA] = basicblock.endEA
+        self._Blocks_list = list(self._Blocks)
+        self._Blocks_list.sort()
+    # startEA:basicblock's start address
+    # return all instruction in one node
+    def get_All_instr_in_one_Node(self,startEA):
+        instr_list = []
+        if (not self._block_boundary.has_key(startEA)):
+            return instr_list
+        
+        endEA = self._block_boundary[startEA]
+        it_code = func_item_iterator_t(self._func, startEA)
+        ea = it_code.current() 
+        while ((ea) < endEA):
+            newlist = []
+            newlist.append(ua_mnem(ea))
+            i = 0
+            op = GetOpnd(ea,i)
+            while not op == "":
+                newlist.append(op)
+                i+=1
+                op = GetOpnd(ea,i)
+                 
+                
+            instr_list.append(newlist)
+            if(not it_code.next_code()):
+                break
+            ea = it_code.current()
+
+        return instr_list
+    
+    #return function's name
     def getFuncName(self):
         return self._name_func
 
@@ -34,20 +76,10 @@ class Process_with_Single_Function(object):
         return GetFrameRegsSize(self._func.startEA)
 
     def getCFG_OF_Func(self):#get the Control Flow Graph of the function , return a list in the form of [(current_block_startaddr:next_block_startaddr), ......]
-        fun_it = func_item_iterator_t(self._func)
-        
-        addr_current = fun_it.current() #now is the start address of the function
-        
-        print hex(addr_current)
-        
-        #fun_it.next_addr() means addr_current+1
-        #fun_it.next_head() means start address of every instruction
-        #fun_it.next_code() just like next_head()
-        #fun_it.next_data() unkown
-        
+        # if a function has only one node , it's cfg may be empty
         #flowchart for a function
-        flowchart = FlowChart(self._func)
         
+        flowchart = FlowChart(self._func)
         list = []
 
         for i in range(flowchart.size):
@@ -59,12 +91,16 @@ class Process_with_Single_Function(object):
             
         return list
 
+    # return all the start address of basicblock in form of set
+    def getAll_Nodes_Addr(self):
+        return self._Blocks_list
 
+    #return a blocks end address
+    def get_Nodes_Endaddr(self,startEA):
+        if (self._block_boundary.has_key(startEA)):
+            return self._block_boundary[startEA]
+        return -1
 
-
-    
-    def Analyzer(self):
-        print 'a'
 #print how to use this script
 def print_help():
     help = 'args not enough'
@@ -82,7 +118,19 @@ def main():
 
         p_func = Process_with_Single_Function(fun)
         print p_func.getFuncName()
-        print p_func.getCFG_OF_Func()
+        #print p_func.getCFG_OF_Func()
+        #print p_func.getAll_Nodes_Addr()
+        #for item in p_func.getAll_Nodes_Addr():
+            #print hex(item),hex(p_func.get_Nodes_Endaddr(item))
+        if (p_func.getFuncName() == 'send_mail'):
+            allnodes = p_func.getAll_Nodes_Addr()
+            for i in range(len(allnodes)):
+                print hex(allnodes[i])
+                instr_list = p_func.get_All_instr_in_one_Node(allnodes[i])
+                print instr_list
+
+
+
 #do something within one function
 
 if __name__ == '__main__':
